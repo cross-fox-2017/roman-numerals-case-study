90f5d87baea2855c3064b4230a271bf582e226c3
Hacktiv8 <hacktiv8-14@Hacktiv8-14.local>
2017-01-03 20:33:19 +0700

    Tugas roman_numerals didit.suryadi

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,36 +1,36 @@
-
-Please answer the questions below :
-
-1. Apakah semua solusi bisa di eksekusi? Bagaimana caranya mem-verifikasinya?
-   Tidak, pada Roman_Numeral3 code tidak jalan saat variabel lebih dari 1000 karena kondisional tidak terpenuhi, value index maximal undefined.
-   Verifikasi dilakukan dengan running script dan melihat hasil output yang diinginkan, jika tidak jalan maka direview kembali codenya.
-
-2. Apakah ada solusi code yang mengandung *bugs*? Dan bagaimana cara mem-verifikasinya?
-   Untuk bugs dilihat dari error code yang timbul, jika tidak muncul erorr code maka dilihat kembali codingnya dapat menggunakan console log
-
-3. Adakah sintaks baru yang belum kamu ketahui? Apakah ada operator baru, expression atau method baru?
-   Ada.
-   1. yaitu penambahan operator + didepan variabel atau fungsi yang ditulis tanpa spasi, terdapat pada numeral2 dan numeral3.
-   2. penggunaan deklarasi varibel dengan typeof
-
-4. Dapatkah kamu mengerti baris per baris dari semua code yang ada?
-   Pada roman_numeral3 baris 13 dan 14 sulit untuk dimengerti operasi yang dilakukan, selebihnya dapat dipahami dengan baik.
-
-5. Dapatkah kamu gunakan sintaks baru yang kamu temui di sini di *project* kamu berikutnya?
-   Dapat, ketika menginginkan perbandingan antara dua variabel yang berurutan.
-
-6. Adakah struktur data baru? Apa saja, tolong sebutkan
-   Ada, terdapat struktur function looping dengan for while if
-
-7. Apa yang dapat kamu pelajari dari perspektif yang berbeda dari berbagai solution code? Apakah benar-benar berbeda? Jika ya, kenapa berbeda? Debat! Cari tau.
-   Ya, sangat berbeda pada penulisan solution code tersebut, karena biasanya penggunaan while..disertai dengan do, tetapi pada solution code tersebut menggunakan metode baru
-   dengan menerapkan struktur while(...){for..{if(..)}}
-   
-8. Jika kamu harus memilih satu, mana yang menurut kamu yang merupakan solsi terbaik, dan kenapa? Diskusikan dengan teman kelompok kamu tentang hal ini.
-   Saya memilih roman_numeral4 karena coding yang sangat efisien dengan algoritma sederhana namun dapat terbaca dengan baik, sehingga tidak menyulitkan debugging.
-
-9. Apakah dampak dari pola atau pattern tersebut terhadap performa aplikasi? Bagaimana kamu mengukur dampaknya?
-   Ketika pola semakin sulit, tentu akan menghabiskan resource yang lebih banyak, mungkin belum terasa ketika code baru sedikit namun jika diimplementasikan dengan fungsi-fungsi lain akan terasa resource yang digunakan.
-
-10. Apakah dampak dari pola atau pattern terhadap maintenance code? Kenapa kamu bisa yakin?
-    Dampak pola atau pattern yang semakin kompleks, akan menimbulkan kebingungan terhadap developer lain yang ingin maintenance, sehingga membutuhkan waktu yang lama untuk mempelajari code perbaris, bahkan mencoba console log untuk mengetahui kegunaannya.
+//
+// Please answer the questions below :
+//
+// 1. Apakah semua solusi bisa di eksekusi? Bagaimana caranya mem-verifikasinya?
+//    Tidak, pada Roman_Numeral3 code tidak jalan saat variabel lebih dari 1000 karena kondisional tidak terpenuhi, value index maximal undefined.
+//    Verifikasi dilakukan dengan running script dan melihat hasil output yang diinginkan, jika tidak jalan maka direview kembali codenya.
+//
+// 2. Apakah ada solusi code yang mengandung *bugs*? Dan bagaimana cara mem-verifikasinya?
+//    Untuk bugs dilihat dari error code yang timbul, jika tidak muncul erorr code maka dilihat kembali codingnya dapat menggunakan console log
+//
+// 3. Adakah sintaks baru yang belum kamu ketahui? Apakah ada operator baru, expression atau method baru?
+//    Ada.
+//    1. yaitu penambahan operator + didepan variabel atau fungsi yang ditulis tanpa spasi, terdapat pada numeral2 dan numeral3.
+//    2. penggunaan deklarasi varibel dengan typeof
+//
+// 4. Dapatkah kamu mengerti baris per baris dari semua code yang ada?
+//    Pada roman_numeral3 baris 13 dan 14 sulit untuk dimengerti operasi yang dilakukan, selebihnya dapat dipahami dengan baik.
+//
+// 5. Dapatkah kamu gunakan sintaks baru yang kamu temui di sini di *project* kamu berikutnya?
+//    Dapat, ketika menginginkan perbandingan antara dua variabel yang berurutan.
+//
+// 6. Adakah struktur data baru? Apa saja, tolong sebutkan
+//    Ada, terdapat struktur function looping dengan for while if
+//
+// 7. Apa yang dapat kamu pelajari dari perspektif yang berbeda dari berbagai solution code? Apakah benar-benar berbeda? Jika ya, kenapa berbeda? Debat! Cari tau.
+//    Ya, sangat berbeda pada penulisan solution code tersebut, karena biasanya penggunaan while..disertai dengan do, tetapi pada solution code tersebut menggunakan metode baru
+//    dengan menerapkan struktur while(...){for..{if(..)}}
+//
+// 8. Jika kamu harus memilih satu, mana yang menurut kamu yang merupakan solsi terbaik, dan kenapa? Diskusikan dengan teman kelompok kamu tentang hal ini.
+//    Saya memilih roman_numeral4 karena coding yang sangat efisien dengan algoritma sederhana namun dapat terbaca dengan baik, sehingga tidak menyulitkan debugging.
+//
+// 9. Apakah dampak dari pola atau pattern tersebut terhadap performa aplikasi? Bagaimana kamu mengukur dampaknya?
+//    Ketika pola semakin sulit, tentu akan menghabiskan resource yang lebih banyak, mungkin belum terasa ketika code baru sedikit namun jika diimplementasikan dengan fungsi-fungsi lain akan terasa resource yang digunakan.
+//
+// 10. Apakah dampak dari pola atau pattern terhadap maintenance code? Kenapa kamu bisa yakin?
+//     Dampak pola atau pattern yang semakin kompleks, akan menimbulkan kebingungan terhadap developer lain yang ingin maintenance, sehingga membutuhkan waktu yang lama untuk mempelajari code perbaris, bahkan mencoba console log untuk mengetahui kegunaannya.
