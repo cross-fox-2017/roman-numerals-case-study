10d705118fdfd5de83b2ca1129f9ae4c9fd2e2f3
Your Name <name@example.com>
2017-01-03 10:15:22 +0700

    Roman Numerals Case Sutdy Done

--- roman_numerals.js
+++ roman_numerals.js
@@ -2,22 +2,23 @@
 Please answer the questions below :
 
 1. Apakah semua solusi bisa di eksekusi? Bagaimana caranya mem-verifikasinya?
-
+   Semua dapat di eksekusi, dengan alur program yang berbeda2, cara mengeceknya dengan melihat hasil dari concole.log apakah sesuai atau tidak
 2. Apakah ada solusi code yang mengandung *bugs*? Dan bagaimana cara mem-verifikasinya?
-
+   Code yang saya buat sudah saya saya buat sesimple mungkin dan sudah saya dengan berbagai percobaan dan belum terjadi masalah
 3. Adakah sintaks baru yang belum kamu ketahui? Apakah ada operator baru, expression atau method baru?
-
+   sintaks yang lama pemahamannya adalah untuk romanNumeral ke 4, karena menggunaan modulus, sedangkan roman numeral pertama adalah yang paling mudah, sekalipun menulisan array baru masih terlalu panjang
 4. Dapatkah kamu mengerti baris per baris dari semua code yang ada?
-
+   dapat dimengerti, hanya code untuk roman numerals ke 4 yang agak lama pemahamannya
 5. Dapatkah kamu gunakan sintaks baru yang kamu temui di sini di *project* kamu berikutnya?
-
+    setelah saya baca-baca hal tersebut bisa dilakukan
 6. Adakah struktur data baru? Apa saja, tolong sebutkan
-
+    para roman numerals ke 2 adalah struktur data baru yang baru saya ketahui, karena modelnya berbeda dari code2 yagn pernah saya tulis sebelumnya
 7. Apa yang dapat kamu pelajari dari perspektif yang berbeda dari berbagai solution code? Apakah benar-benar berbeda? Jika ya, kenapa berbeda? Debat! Cari tau.
-
+    perspektif berbeda dari 4 code tersebut adalah para roman numerals ke 2
 8. Jika kamu harus memilih satu, mana yang menurut kamu yang merupakan solsi terbaik, dan kenapa? Diskusikan dengan teman kelompok kamu tentang hal ini.
-
+  solusi terbaik adalah roman numerals 1 hanya penulisann array tidak perlu sepanjang itu
 9. Apakah dampak dari pola atau pattern tersebut terhadap performa aplikasi? Bagaimana kamu mengukur dampaknya?
-
+  dampak dari algorima setiap program adalah cepat atau lambatnya program itu berjalan sekalipun hanya sepermili detik
 10. Apakah dampak dari pola atau pattern terhadap maintenance code? Kenapa kamu bisa yakin?
+  karena penulisan panjang suatu code akan memakan memory lebih banyak
 */
