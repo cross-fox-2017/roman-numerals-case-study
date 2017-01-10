eb686eb59b36db82d2548f44fca5bf1d14cac473
Dale <dellryuzi@gmail.com>
2017-01-10 23:36:12 +0700

    push

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,23 +1,31 @@
+Crossfox : Dale Watson
 /*
 Please answer the questions below :
 
 1. Apakah semua solusi bisa di eksekusi? Bagaimana caranya mem-verifikasinya?
+   cara mem-verifikasinya dengan meng execute code nya, atau setidaknya melihat return
 
 2. Apakah ada solusi code yang mengandung *bugs*? Dan bagaimana cara mem-verifikasinya?
+  soulsi yg mengandung bugs dengan melihat kembali array yg dipanggil dan cara algoritma bekerjany
 
 3. Adakah sintaks baru yang belum kamu ketahui? Apakah ada operator baru, expression atau method baru?
+  seharusnya tidak dilihat secara cepat
 
 4. Dapatkah kamu mengerti baris per baris dari semua code yang ada?
+   dapat , tetapi yg terakhir masih belum mengerti mengapa harus dimodulus.
 
 5. Dapatkah kamu gunakan sintaks baru yang kamu temui di sini di *project* kamu berikutnya?
+  mungkin
 
 6. Adakah struktur data baru? Apa saja, tolong sebutkan
+  tidak
 
 7. Apa yang dapat kamu pelajari dari perspektif yang berbeda dari berbagai solution code? Apakah benar-benar berbeda? Jika ya, kenapa berbeda? Debat! Cari tau.
-
+  ya benar-benar berbeda yaitu efesiensinya
 8. Jika kamu harus memilih satu, mana yang menurut kamu yang merupakan solsi terbaik, dan kenapa? Diskusikan dengan teman kelompok kamu tentang hal ini.
-
+  yang ke-3
 9. Apakah dampak dari pola atau pattern tersebut terhadap performa aplikasi? Bagaimana kamu mengukur dampaknya?
-
+  dampaknya adalah speed untuk meng-executenya
 10. Apakah dampak dari pola atau pattern terhadap maintenance code? Kenapa kamu bisa yakin?
+  dampaknya lebih mudah di edit dan lebih dinamis, karena semakin code lebih mudah dibaca dan dimengerti maka lebih mudah diedit 
 */
