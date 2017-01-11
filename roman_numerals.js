110ba7232e1217761f64abd2530e35ace7df2506
Irsan Sebastian <email": null>
2017-01-11 19:23:52 +0700

    first commit

--- roman_numerals.js
+++ roman_numerals.js
@@ -3,21 +3,44 @@ Please answer the questions below :
 
 1. Apakah semua solusi bisa di eksekusi? Bagaimana caranya mem-verifikasinya?
 
+  semua bisa dijalankan namun, number 3 tidak bisa dijalankan, ada bugs. dengan console log kita bisa memverifikasi hasilnya
+
+
 2. Apakah ada solusi code yang mengandung *bugs*? Dan bagaimana cara mem-verifikasinya?
+
+    untuk saat ini saya paling mengerti code / solusi yang saya buat sendiri
 
 3. Adakah sintaks baru yang belum kamu ketahui? Apakah ada operator baru, expression atau method baru?
 
+    number 4, menggunakan modulus, tidak kebayang cara bekerjanya
+
 4. Dapatkah kamu mengerti baris per baris dari semua code yang ada?
+
+    overall mengerti
 
 5. Dapatkah kamu gunakan sintaks baru yang kamu temui di sini di *project* kamu berikutnya?
 
+    beberapa syntax dapat digunakan logicnya, seperti penggunaan yang cerdas untuk .pop dari array
+
 6. Adakah struktur data baru? Apa saja, tolong sebutkan
+
+      saya tidak melihat adanya struktur data yang baru
 
 7. Apa yang dapat kamu pelajari dari perspektif yang berbeda dari berbagai solution code? Apakah benar-benar berbeda? Jika ya, kenapa berbeda? Debat! Cari tau.
 
+        kecepatan serta refactor beberapa baris, yang seharusny simple saya buat ribet, jadi saya harus benar2 cermat dalam memiliki logiak mana yang cepat untuk solve this problem
+
+
+
 8. Jika kamu harus memilih satu, mana yang menurut kamu yang merupakan solsi terbaik, dan kenapa? Diskusikan dengan teman kelompok kamu tentang hal ini.
+
+        number 1, karena mudah dibaca dan mudah dimaintain
 
 9. Apakah dampak dari pola atau pattern tersebut terhadap performa aplikasi? Bagaimana kamu mengukur dampaknya?
 
+      kembali ke jawaban nomor 8, mudah dimaintain dan mudah dipelajari
+
 10. Apakah dampak dari pola atau pattern terhadap maintenance code? Kenapa kamu bisa yakin?
+
+      karena polanya mudah dibaca dan dimengerti, dan juga proses PC dalam membaca data pun lebih cepat
 */
